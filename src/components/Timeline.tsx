4d4b717bf4c56412bece0d39d470856bfa75d327
atulsmania <atul14ac@gmail.com>
2023-04-28 03:54:22 +0530

    added timeline year border and journey header

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -9,13 +9,22 @@ type TimelineItem = {
 const Timeline = ({ children }: PropsWithChildren) => {
   return (
     <div className="w-full h-full">
-      <div className="relative h-full overflow-hidden wrap">
+      <div className="relative h-full pt-4 space-y-4 overflow-hidden wrap">
         <div
           className={classNames(
-            "absolute hidden h-full border-2 sm:block",
+            "absolute hidden w-0 h-full border-2 sm:block",
             "left-1/2 border-opacity-20 border-neutral-400"
           )}
-        />
+        >
+          <span
+            className={classNames(
+              "absolute px-4 pt-1 text-xl rounded-full",
+              "bg-neutral-400 text-black -translate-x-1/2 -translate-y-1/2"
+            )}
+          >
+            Journey
+          </span>
+        </div>
         {children}
       </div>
     </div>
@@ -25,14 +34,18 @@ const Timeline = ({ children }: PropsWithChildren) => {
 const Item = ({ children, year, align }: TimelineItem) => {
   return (
     <div
-      className={classNames(
-        "flex items-center justify-between w-full mb-8 left-timeline",
-        { "sm:flex-row-reverse": align === "left" }
-      )}
+      className={classNames("flex items-center justify-between w-full", {
+        "sm:flex-row-reverse": align === "left",
+      })}
     >
       <div className="order-1 hidden w-5/12 sm:block" />
-      <div className="z-20 items-center order-1 hidden p-2 shadow-xl sm:flex aspect-square w-fit">
-        <h1 className="mx-auto text-lg font-semibold">{year}</h1>
+      <div
+        className={classNames(
+          "z-20 items-center order-1 hidden px-2 py-1 bg-black",
+          "border-4 rounded-lg border-neutral-400/20 sm:flex w-fit"
+        )}
+      >
+        <span className="mx-auto text-lg font-semibold">{year}</span>
       </div>
       <div className="order-1 w-full rounded-lg sm:w-5/12">{children}</div>
     </div>
