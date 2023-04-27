14683599df52770f5ddb9a9ef8c252c8153afd63
atulsmania <atul14ac@gmail.com>
2023-04-27 05:46:07 +0530

    timeline detail text size

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
           <h2 className="text-2xl font-bold">B.Tech - Computer Science</h2>
           <h3 className="text-xl">ABES Engineering College</h3>
 
-          <div className="mt-8">
+          <div className="mt-8 text-md">
             Published a research paper on traffic congestion problems and ways
             to solve it. The paper covered topics such as the causes of traffic
             congestion, and possible solutions to reduce congestion. Built a
@@ -22,7 +22,7 @@ const About = () => {
           <h2 className="text-2xl font-bold">Frontend Developer</h2>
           <h3 className="text-xl">Datasigns Technologies - MyShubhLife</h3>
 
-          <div className="mt-8">
+          <div className="mt-8 text-md">
             Built and managed a custom web component library using compound
             pattern, resulting in a 30% reduction in development time for new
             projects. Investigation of app optimization techniques and SPA
